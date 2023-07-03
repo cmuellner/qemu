7a4a5570e893e8833ce13b557bcde3493ff2b6c5
Christoph Müllner <christoph.muellner@vrull.eu>
2023-07-03 14:13:28 +0200

    DO NOT COMMIT: Fixup for AES test
    The out of tree series from Richard Henerson,
    which is required as a dependency for the RISC-V vector crypto
    extension support series, introduces tests for AES using
    the AES instructions.
    
    The compilation of this test is gated by the compiler accepting
    "-mrv64g_zk". As this is not a valid compiler flag (valid would
    be "-march=rv64g_zk"), the AES test will never be executed.
    
    This fixes this by dropping the gate and using the RISC-V-specific
    ".insn" directives to build up the AES instructions.
    
    With this change applied, there is not depenencies to the cross
    toolchain anymore and the test will always be executed.
    
    Signed-off-by: Christoph Müllner <christoph.muellner@vrull.eu>

--- tests/tcg/riscv64/test-aes.c
+++ tests/tcg/riscv64/test-aes.c
@@ -7,8 +7,9 @@ bool test_SB_SR(uint8_t *o, const uint8_t *i)
     uint64_t *o8 = (uint64_t *)o;
     const uint64_t *i8 = (const uint64_t *)i;
 
-    asm("aes64es %0,%2,%3\n\t"
-        "aes64es %1,%3,%2"
+    /* aes64es rd, rs1, rs2 = 0011001 rs2 rs1 000 rd 0110011 */
+    asm(".insn r 0x33, 0x0, 0x19, %0, %2, %3\n\t"
+        ".insn r 0x33, 0x0, 0x19, %1, %3, %2"
         : "=&r"(o8[0]), "=&r"(o8[1]) : "r"(i8[0]), "r"(i8[1]));
     return true;
 }
@@ -24,8 +25,9 @@ bool test_SB_SR_MC_AK(uint8_t *o, const uint8_t *i, const uint8_t *k)
     const uint64_t *i8 = (const uint64_t *)i;
     const uint64_t *k8 = (const uint64_t *)k;
 
-    asm("aes64esm %0,%2,%3\n\t"
-        "aes64esm %1,%3,%2\n\t"
+    /* aesesm rd, rs1, rs2 = 0011011 rs2 rs1 000 rd 0110011 */
+    asm(".insn r 0x33, 0x0, 0x1b, %0, %2, %3\n\t"
+        ".insn r 0x33, 0x0, 0x1b, %1, %3, %2\n\t"
         "xor %0,%0,%4\n\t"
         "xor %1,%1,%5"
         : "=&r"(o8[0]), "=&r"(o8[1])
@@ -38,8 +40,9 @@ bool test_ISB_ISR(uint8_t *o, const uint8_t *i)
     uint64_t *o8 = (uint64_t *)o;
     const uint64_t *i8 = (const uint64_t *)i;
 
-    asm("aes64ds %0,%2,%3\n\t"
-        "aes64ds %1,%3,%2"
+    /* aes64ds rd, rs1, rs2 = 0011101 rs2 rs1 000 rd 0110011 */
+    asm(".insn r 0x33, 0x0, 0x1d, %0, %2, %3\n\t"
+        ".insn r 0x33, 0x0, 0x1d, %1, %3, %2"
         : "=&r"(o8[0]), "=&r"(o8[1]) : "r"(i8[0]), "r"(i8[1]));
     return true;
 }
@@ -49,8 +52,9 @@ bool test_IMC(uint8_t *o, const uint8_t *i)
     uint64_t *o8 = (uint64_t *)o;
     const uint64_t *i8 = (const uint64_t *)i;
 
-    asm("aes64im %0,%0\n\t"
-        "aes64im %1,%1"
+    /* aes64im rd, rs1 = 0011000 00000 rs1 001 rd 0010011 */
+    asm(".insn r 0x13, 0x1, 0x18, %0, %0, x0\n\t"
+        ".insn r 0x13, 0x1, 0x18, %1, %1, x0"
         : "=r"(o8[0]), "=r"(o8[1]) : "0"(i8[0]), "1"(i8[1]));
     return true;
 }
@@ -66,8 +70,9 @@ bool test_ISB_ISR_IMC_AK(uint8_t *o, const uint8_t *i, const uint8_t *k)
     const uint64_t *i8 = (const uint64_t *)i;
     const uint64_t *k8 = (const uint64_t *)k;
 
-    asm("aes64dsm %0,%2,%3\n\t"
-        "aes64dsm %1,%3,%2\n\t"
+    /* aes64dsm rd, rs1, rs2 = 0011111 rs2 rs1 000 rd 0110011 */
+    asm(".insn r 0x33, 0x0, 0x1f, %0, %2, %3\n\t"
+        ".insn r 0x33, 0x0, 0x1f, %1, %3, %2\n\t"
         "xor %0,%0,%4\n\t"
         "xor %1,%1,%5"
         : "=&r"(o8[0]), "=&r"(o8[1])
